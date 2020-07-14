d7d064beb93cfc21469b22a05be2e8dea035b900
eduardolfl <eduardolfl@gmail.com>
2020-07-14 13:47:46 -0400

    Update de readme e test.py

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "Cabecalho da pagina")
+        self.assertRegex(result.data.decode(), "Testando cabecalho da pagina")
 
 
 if __name__ == "__main__":
